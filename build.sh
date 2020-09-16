d0ca3a53720fdfb5a6acb16a2c231b82c91a6937
AaronFacoline <apfacoline@gmail.com>
2020-09-17 01:04:41 +0200

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-cd Server/aegis/
-mvn clean package
-java -jar target/aegis-0.0.1-SNAPSHOT.jar &
-cd frontend/
+cd Server/aegis/frontend/
 npm run build &
 npm start &
 cd ..
+mvn clean package
+java -jar target/aegis-0.0.1-SNAPSHOT.jar &
 cd ..
 cd ..
 cd ServerAIUnityApp/

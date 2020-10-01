a7a455b57e0e3df393fa6d870ea8b66fa6a9c799
Pavlo-Andrianatos <andpavlo@gmail.com>
2020-10-01 23:59:15 +0200

    Added Doxygen comments to LightingPreset.cs

--- Unity Simulation/Pathing2.0/Assets/Scripts/LightingPreset.cs
+++ Unity Simulation/Pathing2.0/Assets/Scripts/LightingPreset.cs
@@ -1,7 +1,14 @@
-﻿using System.Collections;
+﻿/**
+	@file LightingPreset.cs
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+	This class is implemented to hold values concerning the day-night cycle
+*/
 [System.Serializable]
 [CreateAssetMenu(fileName = "Lighting Preset", menuName = "Scriptables/Lighting Preset", order = 1)]
 public class LightingPreset : ScriptableObject
